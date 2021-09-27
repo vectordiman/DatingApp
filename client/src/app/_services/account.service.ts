9215dc7daa59151a19b164f1e4227e9298ebcc73
vectordiman <vector_diman@mail.ru>
2021-09-27 15:43:42 +0300

    End of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map} from 'rxjs/operators'
-import {User} from "../models/user";
+import {User} from "../_models/user";
 import {ReplaySubject} from "rxjs";
 
 @Injectable({
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {User} from "./models/user";
+import {User} from "./_models/user";
 import {AccountService} from "./_services/account.service";
 
 @Component({
