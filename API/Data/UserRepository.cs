d1a4c8a940e33b7d44883a804a597a2439f196cc
vectordiman <vector_diman@mail.ru>
2021-09-29 12:04:02 +0300

    Section 8: Updating the users controller

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -27,7 +27,7 @@ namespace API.Data
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.Include(p => p.Photos).ToListAsync();
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -37,7 +37,7 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
-            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+            return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == username);
         }
     }
 }
