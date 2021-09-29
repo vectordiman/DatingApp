9e17554a4ac7ae7f05d46e579f7d422ab79029cf
vectordiman <vector_diman@mail.ru>
2021-09-29 11:29:32 +0300

    Section 8: Seeding data

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -18,13 +19,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
         
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
