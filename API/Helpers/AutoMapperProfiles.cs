777c2e9831e8756b25765e70394b35b14ffb5a34
vectordiman <vector_diman@mail.ru>
2021-09-30 15:13:45 +0300

    Section 10: Persisiting the changes in the API

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -19,6 +19,8 @@ namespace API.Helpers
                         src => src.DateOfBirth.CalculateAge()));
             
             CreateMap<Photo, PhotoDto>();
+
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
