06b98fa4e545741597031efbca9ae251840d4d44
vectordiman <vector_diman@mail.ru>
2021-10-04 18:21:59 +0300

    Section 12: Adding custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountService} from "../_services/account.service";
 import {ToastrService} from "ngx-toastr";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -23,9 +23,20 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       username: new FormControl('',Validators.required),
       password: new FormControl('',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('',Validators.required)
+      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])
+    })
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
     })
   }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      // @ts-ignore
+      return control?.value !== control?.parent?.controls[matchTo].value ? {isMatching: true} : null;
+    }
+  }
+
 
   register() {
 
