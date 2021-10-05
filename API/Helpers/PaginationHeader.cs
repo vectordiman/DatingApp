7af4e56856665247f945a2e8bae2a1049ff7524a
vectordiman <vector_diman@mail.ru>
2021-10-05 09:46:56 +0300

    Section 13: Adding helper classes for pagination

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,21 @@
+﻿namespace API.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int currentPage, int itemsPerPages, int totalItems, int totalPages)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPages = itemsPerPages;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        }
+
+        public int CurrentPage { get; set; }
+
+        public int ItemsPerPages { get; set; }
+        
+        public int TotalItems { get; set; }
+        
+        public int TotalPages { get; set; }
+    }
+}
