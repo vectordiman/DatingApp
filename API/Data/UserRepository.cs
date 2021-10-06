bb86a47e6732462d9c9804c371ff887f1c274813
vectordiman <vector_diman@mail.ru>
2021-10-06 16:29:23 +0300

    Section 13: Adding additional filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
@@ -55,6 +56,11 @@ namespace API.Data
             query = query.Where(u => u.UserName != userParams.CurrentUsername);
             query = query.Where(u => u.Gender == userParams.Gender);
 
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            
             return await PagedList<MemberDto>.CreateAsynk(
                 query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(), userParams.PageNumber,
                 userParams.PageSize);
