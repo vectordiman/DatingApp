cae9e68fb9fd601ced0542d3a63a178e4994cb62
vectordiman <vector_diman@mail.ru>
2021-10-08 11:22:09 +0300

    Section 14: Adding a likes repository

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class LikesRepository: ILikesRepository
+    {
+        private readonly DataContext _context;
+
+        public LikesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<UserLike> GetUserLike(int sourceUserId, int likerUserId)
+        {
+            
+        }
+
+        public Task<AppUser> GetUserWithLikes(int userId)
+        {
+            
+        }
+
+        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            
+        }
+    }
+}
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public class ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likerUserId);
+
+        Task<AppUser> GetUserWithLikes(int userId);
+
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+}
