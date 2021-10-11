4fbe17c01cc1b3531eb423d06fb031e508af0de6
vectordiman <vector_diman@mail.ru>
2021-10-11 15:40:37 +0300

    Section 15: Sending messages

--- client/src/app/member-messages/member-messages.component.ts
+++ client/src/app/member-messages/member-messages.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../_models/message";
 import {MessageService} from "../_services/message.service";
+import {MembersService} from "../_services/members.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-member-messages',
@@ -8,13 +10,20 @@ import {MessageService} from "../_services/message.service";
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm!: NgForm;
   @Input() messages!: Message[];
   @Input() username!: string;
+  messageContent!: string;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {}
 
-
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
+      this.messages.push(message);
+      this.messageForm.reset();
+    })
+  }
 
 }
