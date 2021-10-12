adaee6534bd2060a75737aede50786d8056aa49f
vectordiman <vector_diman@mail.ru>
2021-10-12 16:10:39 +0300

    Section 16: Adding the roles to the JWT token

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,7 +31,7 @@ namespace API.Controllers
             _mapper = mapper;
             _photoService = photoService;
         }
-
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
@@ -46,7 +46,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
-
+        
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
