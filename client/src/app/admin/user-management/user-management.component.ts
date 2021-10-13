9f45131c891bbb3cf60799b1d1659a7159825289
vectordiman <vector_diman@mail.ru>
2021-10-13 16:12:16 +0300

    Section 16: Setting up modals

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../../_models/user";
 import {AdminService} from "../../_services/admin.service";
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {RolesModalComponent} from "../../modals/roles-modal/roles-modal.component";
 
 @Component({
   selector: 'app-user-management',
@@ -9,8 +11,9 @@ import {AdminService} from "../../_services/admin.service";
 })
 export class UserManagementComponent implements OnInit {
   users!: Partial<User[]>;
+  bsModalRef!: BsModalRef;
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.getUsersWithRoles();
@@ -20,6 +23,28 @@ export class UserManagementComponent implements OnInit {
     this.adminService.getUsersWithRoles().subscribe(users => {
       this.users = users;
       console.log(this.users);
+    })
+  }
+
+  openRolesModal() {
+    const config = {
+      class: 'modal-dialog-centered',
+      initialState: {
+        // user,
+        // roles: this.getRolesArray(user)
+      }
+    }
+    // this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+    this.bsModalRef = this.modalService.show(RolesModalComponent);
+    this.bsModalRef.content.updateSelectedRoles.subscribe((values: any[]) => {
+      const rolesToUpdate = {
+        roles: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+      // if (rolesToUpdate) {
+      //   this.adminService.updateUserRoles(user.username, rolesToUpdate.roles).subscribe(() => {
+      //     user.roles = [...rolesToUpdate.roles]
+      //   })
+      // }
     })
   }
 
