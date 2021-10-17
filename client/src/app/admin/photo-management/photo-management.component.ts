369cded5967326cc37afedf4dc48f8b9987c35ee
vectordiman <vector_diman@mail.ru>
2021-10-17 22:32:18 +0300

    Extra: Adding photo management in client

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminService} from "../../_services/admin.service";
+import {Photo} from "../../_models/photo";
 
 @Component({
   selector: 'app-photo-management',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-management.component.css']
 })
 export class PhotoManagementComponent implements OnInit {
+  photos!: Photo[];
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.getPhotosForApproval();
   }
 
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe(photos => {
+      this.photos = photos;
+    })
+  }
+
+  approvePhoto(photoId: number) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    })
+  }
+
+  rejectPhoto(photoId: number) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    })
+  }
 }
